1c7f1c521e29b88d05f52c7c0506255e7967bff1
Shoeb <shoebahmed@yahoo.com>
2020-01-01 15:04:01 +0530

    Initial Commit

--- js/contactform.js
+++ js/contactform.js
@@ -1,21 +1,20 @@
-(function ($) {   
+(function ($) { 
 	jQuery('#contact-form').click(function () {
-		var formData = new FormData($('#contactUsForm')[0]);
+		//var formData = new FormData($('#contactUsForm')[0]);
     	const apiData = { firstName: contactUsForm.firstName.value, 
-						  lastName: contactUsForm.lastName.value,
-						  email: contactUsForm.email.value, 
-						  phoneNumber: contactUsForm.phoneNumber.value,
-						  address: contactUsForm.address.value, 
-						  chooseOption: contactUsForm.chooseOption.value,
-						  message: contactUsForm.message.value,
-							userId: contactUsForm.USERID.value};
-			
+                          lastName: contactUsForm.lastName.value,
+                          email: contactUsForm.email.value, 
+                          phoneNumber: contactUsForm.phoneNumber.value,
+                          address: contactUsForm.address.value, 
+                          chooseOption: contactUsForm.chooseOption.value,
+                          message: contactUsForm.message.value,
+						  userId: contactUsForm.USERID.value
+						  };
 		var formData = JSON.stringify(apiData); 
-    	//alert(formData);
-		
+    	// alert(formData);
     	$.ajax(
         {
-            url : "http://localhost:8082/email/contactform",
+            url : "http://35.182.103.129/email/contactform",
             type: "POST",
             crossDomain: true,
             data: formData,
@@ -24,14 +23,14 @@
             processData: false,
             success:function(data, textStatus, jqXHR){
                 //data: return data from server
-                alert('it worked');
-               
+                // alert('it worked');
+                swal("Success", "We have received your message. Atlantic Prepaid representative will contact you shortly.", "success");
                 //courseId = data;
             	
             },error: function(jqXHR, textStatus, errorThrown){
                 //if fails   
-                alert('it didnt work');  
-               
+                swal("Error", "Some error has occured. Please try again later.", "error"); 
+                
             }
         });
         
