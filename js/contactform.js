4d1b04b844a7bcbfb58437dcef553474b2879ddf
Shoeb <shoebahmed@yahoo.com>
2020-01-19 17:32:32 +0530

    Initial Commit

--- js/contactform.js
+++ js/contactform.js
@@ -6,7 +6,6 @@
             var last_name = $('#lastName').val();
             var email = $('#email').val();
 
-           
             $(".error").remove();
 
             if (first_name.length < 1) {
@@ -39,10 +38,8 @@
                           message: contactUsForm.message.value,
 						              userId: contactUsForm.USERID.value
                           };
-            
-
 		var formData = JSON.stringify(apiData); 
-        alert(formData);
+        // alert(formData);
         if(valid_count == 0){
             $.ajax(
                 {
@@ -71,48 +68,3 @@
         event.preventDefault(); //STOP default action
 	}); 
 })(jQuery);
-
-
-
-
-// (function ($) { 
-// 	jQuery('#contact-form').click(function () {
-//     const apiData = {   firstName: contactUsForm.firstName.value, 
-//       lastName: contactUsForm.lastName.value,
-//       email: contactUsForm.email.value, 
-//       phoneNumber: contactUsForm.phoneNumber.value,
-//       address: contactUsForm.address.value, 
-//       chooseOption: contactUsForm.chooseOption.value,
-//       message: contactUsForm.message.value,
-//       userId: contactUsForm.USERID.value
-//       };
-
-
-//   var formData = JSON.stringify(apiData); 
-//   alert(formData);
-//       $.ajax(
-//         {
-//             url : "http://35.182.103.129/email/contactform",
-//             type: "POST",
-//             crossDomain: true,
-//             data: formData,
-//             //use contentType, processData for sure.
-//             contentType : "application/json",
-//             processData: false,
-//             success:function(data, textStatus, jqXHR){
-//                 //data: return data from server
-//                 // alert('it worked');
-//                 swal("Success", "We have received your message. Atlantic Prepaid representative will contact you shortly.", "success");
-//                 //courseId = data;
-//                 var frm = document.getElementsByName('contactUsForm')[0];
-//                 frm.reset();
-//             },error: function(jqXHR, textStatus, errorThrown){
-//                 //if fails   
-//                 swal("Error", "Some error has occured. Please try again later.", "error"); 
-                
-//             }
-//         });
-     
-//       event.preventDefault(); //STOP default action
-// }); 
-// })(jQuery);
